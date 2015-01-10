f0b59c8f6a1c9bdf3f6ed16d1a40c970ecb08286
rizzi <rizzi@rizzitherm.com.br>
2015-01-10 09:21:04 -0200

    Tenants ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,31 +9,31 @@ user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
 
 
-Unidade.create( nome: 'litro', sigla: 'lt')
-Unidade.create(nome: 'kilograma', sigla: 'kg')
-Unidade.create(nome: 'tonelada', sigla: 'ton')
+Unidade.create( id: 1, nome: 'litro', sigla: 'lt')
+Unidade.create( id: 2,  nome: 'kilograma', sigla: 'kg')
+Unidade.create( id: 3,  nome: 'tonelada', sigla: 'ton')
 
-Mp.create( nome: 'Hipoclorito de Sódio (NaCIO)', apelido: 'Cloro', unidade_id: 1)
-Mp.create( nome: 'Acido Sulfurido', apelido: 'Sulfurico', unidade_id: 1)
-Mp.create( nome: 'Acido Acetico', apelido: 'Acetico', unidade_id: 1 )
-Mp.create( nome: 'H2O', apelido: 'Agua Industrial', unidade_id: 1 )
+Mp.create( id: 1, nome: 'Hipoclorito de Sódio (NaCIO)', apelido: 'Cloro', unidade_id: 1)
+Mp.create( id: 2, nome: 'Acido Sulfurido', apelido: 'Sulfurico', unidade_id: 1)
+Mp.create( id: 3, nome: 'Acido Acetico', apelido: 'Acetico', unidade_id: 1 )
+Mp.create( id: 4, nome: 'H2O', apelido: 'Agua Industrial', unidade_id: 1 )
 
 
 
-Formula.create( nome: 'detergente A' )
-Formula.create( nome: 'detergente B')
-Formula.create( nome: 'Limpa Vidro')
-Formula.create( nome: 'Água Sanitária')
-Formula.create( nome: 'Limpa Pedra')
+Formula.create( id: 1, nome: 'detergente A' )
+Formula.create( id: 2, nome: 'detergente B')
+Formula.create( id: 3, nome: 'Limpa Vidro')
+Formula.create( id: 4, nome: 'Água Sanitária')
+Formula.create( id: 5, nome: 'Limpa Pedra')
 
-Tipo.create( tipo: 'Garrafa')
-Tipo.create( tipo: 'Galão')
-Tipo.create( tipo: 'Tambor')
-Tipo.create( tipo: 'Balde')
+Tipo.create( id: 1, tipo: 'Garrafa')
+Tipo.create( id: 2, tipo: 'Galão')
+Tipo.create( id: 3, tipo: 'Tambor')
+Tipo.create( id: 4, tipo: 'Balde')
 
-Material.create( tipo: 'Plástico')
-Material.create( tipo: 'Vidro')
-Material.create( tipo: 'Pet')
+Material.create( id: 1, tipo: 'Plástico')
+Material.create( id: 2, tipo: 'Vidro')
+Material.create( id: 3, tipo: 'Pet')
 
 
 
