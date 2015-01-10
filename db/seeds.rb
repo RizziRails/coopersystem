8b53ff82193280cf082b0260039bb97324f38bc9
rizzi <rizzi@rizzitherm.com.br>
2015-01-10 08:44:58 -0200

    deixando seeds inativo

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,31 +8,31 @@
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
 
-Unidade.create(nome: 'litro', sigla: 'lt')
-Unidade.create(nome: 'kilograma', sigla: 'kg')
-Unidade.create(nome: 'tonelada', sigla: 'ton')
+#Unidade.create( nome: 'litro', sigla: 'lt')
+#Unidade.create(nome: 'kilograma', sigla: 'kg')
+#Unidade.create(nome: 'tonelada', sigla: 'ton')
 
-Mp.create( nome: 'Cloro', apelido: 'Cloro', unidade_id: 1)
-Mp.create( nome: 'Acido Sulfurido', apelido: 'Sulfurico', unidade_id: 1)
-Mp.create( nome: 'Acido Acetico', apelido: 'Acetico', unidade_id: 1 )
-Mp.create( nome: 'H20', apelido: 'Agua', unidade_id: 1 )
+#Mp.create( nome: 'Hipoclorito de Sódio (NaCIO)', apelido: 'Cloro', unidade_id: 1)
+#Mp.create( nome: 'Acido Sulfurido', apelido: 'Sulfurico', unidade_id: 1)
+#Mp.create( nome: 'Acido Acetico', apelido: 'Acetico', unidade_id: 1 )
+#Mp.create( nome: 'H2O', apelido: 'Agua Industrial', unidade_id: 1 )
 
 
 
-Formula.create( nome: 'detergente A' )
-Formula.create( nome: 'detergente B')
-Formula.create( nome: 'Limpa Vidro')
-Formula.create( nome: 'Água Sanitária')
-Formula.create( nome: 'Limpa Pedra')
+#Formula.create( nome: 'detergente A' )
+#Formula.create( nome: 'detergente B')
+#Formula.create( nome: 'Limpa Vidro')
+#Formula.create( nome: 'Água Sanitária')
+#Formula.create( nome: 'Limpa Pedra')
 
-Tipo.create( tipo: 'Garrafa')
-Tipo.create( tipo: 'Galão')
-Tipo.create( tipo: 'Tambor')
-Tipo.create( tipo: 'Balde')
+#Tipo.create( tipo: 'Garrafa')
+#Tipo.create( tipo: 'Galão')
+#Tipo.create( tipo: 'Tambor')
+#Tipo.create( tipo: 'Balde')
 
-Material.create( tipo: 'Plástico')
-Material.create( tipo: 'Vidro')
-Material.create( tipo: 'Pet')
+#Material.create( tipo: 'Plástico')
+#Material.create( tipo: 'Vidro')
+#Material.create( tipo: 'Pet')
 
 
 
