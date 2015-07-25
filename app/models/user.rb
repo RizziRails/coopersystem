10e38342db9894bd1f995abbcc8d52ed0763d7c4
rizzi <rizzi@rizzitherm.com.br>
2015-07-25 17:19:17 -0300

    Adicionado os usuarios em models/user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
-  enum role: [:user, :vip, :admin]
+  enum role: [:user, :user_quimico , :user_comprador, :user_vendedor, :user_administrativo, :logistica, :vip_qualidade, :vip_producao, :vip_logistica, :admin , :mater ]
+  
+  
   after_initialize :set_default_role, :if => :new_record?
 
   def set_default_role
