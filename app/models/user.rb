ec4aad54d5d33ce50c8f665d89393438a05a3ef8
rizzi <rizzi@rizzitherm.com.br>
2015-07-26 10:26:23 -0300

    tudo resolvido com os tipos de usuarios

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  enum role: [:user, :user_quimico , :user_comprador, :user_vendedor, :user_administrativo, :vip_qual, :vip_prod, :vip_log, :vip, :admin , :mater ]
+  enum role: [:user, :user_quimico , :user_comprador, :user_vendedor, :user_administrativo, :vip_qual, :vip_prod, :vip_log, :vip, :admin , :master ]
   
   
   after_initialize :set_default_role, :if => :new_record?
